6e48b464aac00d1bc41272bdee28e3208ac01698
Victor Santos <v.santos@itsoluclick.com>
2024-04-11 17:11:36 -0400

    adding missing files

---
+++ src/types/apps/ncfTypes.ts
@@ -0,0 +1,12 @@
+export interface NcfType {
+  id: number
+  tipoCliente: string
+  descripcion: string
+  encabezado: string
+  secuenciaIni: number
+  secuenciaFin: number
+  secuencia: number
+  vendedor: string
+  vencimiento: string
+  businessId: string
+}
