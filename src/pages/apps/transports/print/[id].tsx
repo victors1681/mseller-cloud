9bd26e7a4f51ffc5e65b25e9d6d2f01d2495d444
Victor Santos <v.santos@itsoluclick.com>
2024-08-17 14:48:25 -0400

    using get server props instead of static pros

--- src/pages/apps/transports/print/[id].tsx
+++ src/pages/apps/transports/print/[id].tsx
@@ -5,39 +5,49 @@ import {
   GetStaticPropsContext,
   InferGetStaticPropsType,
 } from 'next/types'
-
+import { GetServerSidePropsContext } from 'next';
 // ** Third Party Imports
 import axios from 'axios'
 
 // ** Demo Components Imports
 import Print from 'src/views/apps/transports/print/PrintPage'
 
+
 const InvoicePrint = ({
   id,
-}: InferGetStaticPropsType<typeof getStaticProps>) => {
+}: InferGetStaticPropsType<any>) => {
   return <Print id={id} />
 }
 
-export const getStaticPaths: GetStaticPaths = async () => {
-  // const res = await axios.get('/apps/invoice/invoices')
-  // const data: InvoiceType[] = await res.data.allData
-  const data = [] as any
-  // const paths = data.map((item: any) => ({
-  //   params: { id: `${item.id}` },
-  // }))
+// export const getStaticPaths: GetStaticPaths = async () => {
+//   // const res = await axios.get('/apps/invoice/invoices')
+//   // const data: InvoiceType[] = await res.data.allData
+//   const data = [] as any
+//   // const paths = data.map((item: any) => ({
+//   //   params: { id: `${item.id}` },
+//   // }))
 
-  return {
-    paths: [],
-    fallback: true,
-  }
-}
+//   return {
+//     paths: [],
+//     fallback: true,
+//   }
+// }
 
-export const getStaticProps: GetStaticProps = ({
-  params,
-}: GetStaticPropsContext) => {
+// export const getStaticProps: GetStaticProps = ({
+//   params,
+// }: GetStaticPropsContext) => {
+//   return {
+//     props: {
+//       id: params?.id,
+//     },
+//   }
+// }
+
+export async function getServerSideProps(context: GetServerSidePropsContext) {
+
   return {
     props: {
-      id: params?.id,
+      id: context?.params?.id || '',
     },
   }
 }
--- src/pages/apps/transports/printDeliveryReportAmount/[id].tsx
+++ src/pages/apps/transports/printDeliveryReportAmount/[id].tsx
@@ -1,5 +1,6 @@
 // ** Next Import
 import {
+  GetServerSidePropsContext,
   GetStaticPaths,
   GetStaticProps,
   GetStaticPropsContext,
@@ -12,32 +13,38 @@ import axios from 'axios'
 // ** Demo Components Imports
 import Print from 'src/views/apps/transports/printDeliveryReportAmount/PrintPage'
 
-const InvoicePrint = ({
-  id,
-}: InferGetStaticPropsType<typeof getStaticProps>) => {
+const InvoicePrint = ({ id }: InferGetStaticPropsType<any>) => {
   return <Print id={id} />
 }
 
-export const getStaticPaths: GetStaticPaths = async () => {
-  // const res = await axios.get('/apps/invoice/invoices')
-  // const data: InvoiceType[] = await res.data.allData
-  // const data = [] as any
-  // const paths = data.map((item: any) => ({
-  //   params: { id: `${item.id}` },
-  // }))
+// export const getStaticPaths: GetStaticPaths = async () => {
+//   // const res = await axios.get('/apps/invoice/invoices')
+//   // const data: InvoiceType[] = await res.data.allData
+//   // const data = [] as any
+//   // const paths = data.map((item: any) => ({
+//   //   params: { id: `${item.id}` },
+//   // }))
 
-  return {
-    paths: [],
-    fallback: true,
-  }
-}
+//   return {
+//     paths: [],
+//     fallback: true,
+//   }
+// }
 
-export const getStaticProps: GetStaticProps = ({
-  params,
-}: GetStaticPropsContext) => {
+// export const getStaticProps: GetStaticProps = ({
+//   params,
+// }: GetStaticPropsContext) => {
+//   return {
+//     props: {
+//       id: params?.id,
+//     },
+//   }
+// }
+
+export async function getServerSideProps(context: GetServerSidePropsContext) {
   return {
     props: {
-      id: params?.id,
+      id: context?.params?.id || '',
     },
   }
 }
