8b356fa1e71068f91e3e3d319f357149bcb85d0e
Victor Santos <v.santos@itsoluclick.com>
2024-08-17 13:47:34 -0400

    testing static path issue

--- src/pages/apps/transports/docs/[id].tsx
+++ src/pages/apps/transports/docs/[id].tsx
@@ -10,35 +10,37 @@ import {
 
 // ** Demo Components Imports
 import Docs from './index'
+import { useRouter } from 'next/router'
 
-const InvoicePreview = ({
-  id,
-}: InferGetStaticPropsType<typeof getStaticProps>) => {
-  return <Docs noTransporte={id} />
+const InvoicePreview = ({ id }: InferGetStaticPropsType<any>) => {
+  const router = useRouter()
+  const query = router.query
+  console.log(query)
+  return <Docs noTransporte={(query.id as string) || ''} />
 }
 
-export const getStaticPaths: GetStaticPaths = async () => {
-  //const res = await axios.get('/api/transport/transports')
-  const data = [] as any //: InvoiceType[] = await res.data.allData
+// export const getStaticPaths: GetStaticPaths = async () => {
+//   //const res = await axios.get('/api/transport/transports')
+//   const data = [] as any //: InvoiceType[] = await res.data.allData
 
-  const paths = data.map((item: any) => ({
-    params: { id: `${item.id}` },
-  }))
+//   const paths = data.map((item: any) => ({
+//     params: { id: `${item.id}` },
+//   }))
 
-  return {
-    paths,
-    fallback: true,
-  }
-}
+//   return {
+//     paths,
+//     fallback: true,
+//   }
+// }
 
-export const getStaticProps: GetStaticProps = ({
-  params,
-}: GetStaticPropsContext) => {
-  return {
-    props: {
-      id: params?.id,
-    },
-  }
-}
+// export const getStaticProps: GetStaticProps = ({
+//   params,
+// }: GetStaticPropsContext) => {
+//   return {
+//     props: {
+//       id: params?.id,
+//     },
+//   }
+// }
 
 export default InvoicePreview
